40abee1cfb1a208a098e9ecd4e39911a3ccb91ab
Garrett Rodriguez <eloso79@gmail.com>
2014-10-13 10:53:49 -0700

    Finish copy template alert GIT ADD . !

--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -1,11 +1,11 @@
 $(".templates").ready(function() {
-	
+
 	var clip = new ZeroClipboard($("#copy-button"));
-	
 	clip.on( "copy", function(event){
 		//var w = window.open();
 		var clipboard = event.clipboardData;
 		var text = '';
+
 		$('form p').children().each(function(){
 			if($(this).is('label')){
 				text += $(this).text() +' ';
@@ -13,14 +13,20 @@ $(".templates").ready(function() {
 				text += $(this).val() + '\n';
 			}
 		});
+		var modalText = text.replace("\n", "<br />");
+
 		clipboard.setData( "text/plain", text);
-		//w.document.write(text);
+		clipboard.setData( "text/html", modalText);
+		console.log("plain: " + text);
+		console.log("modal: " + modalText);
+		$("#myModal").html("Template Created!</br>Right click and Paste or CTRL+V to insert into HPI</br>" + modalText);
+
 	});
 
-	clip.on( "aftercopy", function(event){
-		alert("Template Created!\nRight click and Paste or CTRL+V to insert into HPI\n\n" + event.data['text/plain']);
-
-	});
+	/*clip.on( "aftercopy", function(event){
+		//alert("Template Created!\nRight click and Paste or CTRL+V to insert into HPI\n\n" + event.data['text/plain']);
+		//$("#myModal").html("Template Created!</br>Right click and Paste or CTRL+V to insert into HPI</br>" + event.data['text/html']);
+	});*/
 	/*$('#copy-button').click(function(){
 		//var w = window.open();
 		var clip = new ZeroClipboard($("#copy-button"));
